135a8958c69f76a6fca70b1fa464e15805b7cb48
abdulsametsahin <41396774+abdulsametsahin@users.noreply.github.com>
2018-11-11 10:41:08 +0300

    Update apache_vhost.sh

--- apache_vhost.sh
+++ apache_vhost.sh
@@ -5,16 +5,15 @@ sudo mkdir -p /var/www/$HOST/public_html
 sudo chown -R $USER:$USER /var/www/$HOST/public_html
 sudo chmod -R 755 /var/www
 sudo echo "it works" >> /var/www/$HOST/public_html/index.html
-sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$HOST.conf
 
-sudo echo '<VirtualHost *:80>
+sudo echo "<VirtualHost *:80>
     ServerAdmin admin@$HOST
     ServerName $HOST
     ServerAlias www.$HOST
     DocumentRoot /var/www/$HOST/$PUBLIC_FOLDER
     ErrorLog ${APACHE_LOG_DIR}/error.log
     CustomLog ${APACHE_LOG_DIR}/access.log combined
-</VirtualHost>' >> /etc/apache2/sites-available/$HOST.conf
+</VirtualHost>" >> /etc/apache2/sites-available/$HOST.conf
 
 sudo a2ensite $HOST.conf
 sudo a2dissite 000-default.conf
