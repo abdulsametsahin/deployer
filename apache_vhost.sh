89f8536809eade8996eb6dc773d484220830f43d
abdulsametsahin <41396774+abdulsametsahin@users.noreply.github.com>
2018-11-12 10:22:31 +0300

    Update apache_vhost.sh

--- apache_vhost.sh
+++ apache_vhost.sh
@@ -1,21 +1,21 @@
-HOST="$1"
+DOMAIN="$1"
 PUBLIC_FOLDER="$2"
 
-sudo mkdir -p /var/www/$HOST/$PUBLIC_FOLDER
-sudo chown -R $USER:$USER /var/www/$HOST
+sudo mkdir -p /var/www/$DOMAIN/$PUBLIC_FOLDER
+sudo chown -R $USER:$USER /var/www/$DOMAIN
 sudo chmod -R 755 /var/www
-touch /var/www/$HOST/$PUBLIC_FOLDER/index.html
-sudo echo "it works" >> /var/www/$HOST/$PUBLIC_FOLDER/index.html
+touch /var/www/$DOMAIN/$PUBLIC_FOLDER/index.html
+sudo echo "it works $DOMAIN" >> /var/www/$DOMAIN/$PUBLIC_FOLDER/index.html
 
 sudo echo "<VirtualHost *:80>
-    ServerAdmin admin@$HOST
-    ServerName $HOST
-    ServerAlias www.$HOST
-    DocumentRoot /var/www/$HOST/$PUBLIC_FOLDER
+    ServerAdmin admin@$DOMAIN
+    ServerName $DOMAIN
+    ServerAlias www.$DOMAIN
+    DocumentRoot /var/www/$DOMAIN/$PUBLIC_FOLDER
     ErrorLog ${APACHE_LOG_DIR}/error.log
     CustomLog ${APACHE_LOG_DIR}/access.log combined
-</VirtualHost>" >> /etc/apache2/sites-available/$HOST.conf
+</VirtualHost>" >> /etc/apache2/sites-available/$DOMAIN.conf
 
-sudo a2ensite $HOST.conf
+sudo a2ensite $DOMAIN.conf
 sudo a2dissite 000-default.conf
 sudo service apache2 restart
